8e7860649ec9f051945c1ecac321ba7026f5d258
Zhilin21-IST-4 <94217280+Zhilin21-IST-4@users.noreply.github.com>
2022-05-08 18:31:55 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -1,20 +1,71 @@
-// Copyright 2022 NNTU-CS
-#ifndef INCLUDE_TRAIN_H_
-#define INCLUDE_TRAIN_H_
+// Copyright 2021 NNTU-CS
+#include "train.h"
 
-class Train {
- private:
-  struct Cage {
-    bool light; // состояние лампочки
-    Cage *next;
-    Cage *prev;
-  };
-  int countOp; // счетчик шагов (число переходов из вагона в вагон)
-  Cage *first; // точка входа в поезд (первый вагон)
- public:
-  Train();
-  void addCage(bool light); // добавить вагон с начальным состоянием лампочки
-  int getLength();          // вычислить длину поезда
-  int getOpCount();         // вернуть число переходов (из вагона в вагон)
-};
-#endif  // INCLUDE_TRAIN_H_
+Train::Train() {
+  first = nullptr;
+  last = nullptr;
+  countOp = 0;
+
+  dlina = 0;
+  var_a = 0;
+}
+
+void Train::addCage(bool light) {
+  if (!(first)) {
+    Cage* NewCage = new Cage;
+    NewCage->light = light;
+   
+    NewCage->next = NewCage->previous = nullptr;
+    first = NewCage;
+    last = first;
+  } else {
+    Cage* NewCage = new Cage;
+    NewCage->light = light;
+
+    NewCage->next = NewCage->previous = nullptr;
+    last->next = NewCage;
+    last->next->previous = last;
+    last = last->next;
+    if (!last->next) {
+      last->next = first;
+    } else {
+      first->previous = last;
+    }
+  }
+}
+int Train::getLength() {
+  int tempp;
+  first->light = true;
+  last = first;
+  while (true) {
+
+    countOp++;
+    var_a++;
+    last = last->next;
+    if (last->light) {
+      tempp = var_a;
+      last->light = false;
+      if ((last->previous) != nullptr) {
+        while (last->light == false) {
+          last = last->previous;
+
+          var_a--;
+          countOp++;
+        }
+      }
+      if (!last->light) {
+
+        dlina = tempp;
+        break;
+      }
+    }
+  }
+
+  countOp += dlina;
+  return dlina;
+}
+
+
+int Train::getOpCount() {
+  return countOp;
+}
